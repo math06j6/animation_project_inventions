cc629d9668afe89bbd6389bb12191ccae599b92b
mathias <math06j6@stud.kea.dk>
2020-03-18 14:33:35 +0100

    svg test

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 "use strict";
 
 document.querySelector("h1").textContent = "Goddag";
+
+const data = [
+  {
+    i: "beachball.svg"
+  }
+];
+data.forEach(d => {
+  const t = document.createElement("svg");
+  t.src = "svg/" + d.i;
+  document.body.appendChild(t);
+});
