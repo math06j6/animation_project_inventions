9196e0923c3d3c37adbf7fe80a16c089a526aa3c
mathias <math06j6@stud.kea.dk>
2020-03-21 17:31:33 +0100

    intersectionObserver v1

--- script.js
+++ script.js
@@ -13,3 +13,42 @@
 //   document.body.appendChild(t);
 // });
 
+window.addEventListener("DOMContentLoaded", init);
+
+const HTML = {};
+
+function init() {
+  console.log("init");
+  HTML.container = document.querySelector("main");
+}
+
+const articles = document.querySelectorAll("article");
+
+// The Intersection Observer
+// Inspiration from MDN and the article "Beautiful Scrolling Experiences – Without Libraries"
+// Link: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+// Link: https://24ways.org/2019/beautiful-scrolling-experiences-without-libraries/
+
+let options = {
+  rootMargin: "0px",
+  threshold: 0.75
+};
+
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    const { target } = entry;
+
+    if (entry.intersectionRatio >= 0.75) {
+      target.classList.add("is-visible");
+      target.classList.add("is-visible");
+    } else {
+      target.classList.remove("is-visible");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+articles.forEach((article, index) => {
+  observer.observe(article);
+});
